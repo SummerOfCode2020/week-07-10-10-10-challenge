d2ed016af8dafde9e218265ac8cae0a47e587f9a
Nick Lavery <tuneboynick@gmail.com>
2020-07-23 17:43:10 -0400

    Starting off with general practice for the end of the week

---
+++ index2.js
@@ -0,0 +1,15 @@
+// More practice for the end of week 8, will likely include tons of array work later
+
+// Reminding myself to be patient with my progress and celebrate the small wins.
+
+
+// Making a function that turns strings into not oriented phrases.
+/*
+function notString(str){
+  if (!str.includes('not')) {
+      return 'not ' + str}
+  else if (str.charAt(0) !== 'n') {
+      return 'not ' + str}
+  else if (str.includes('not')) {
+      return str}
+*/
