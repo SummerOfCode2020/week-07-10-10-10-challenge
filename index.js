9eb7a471ff7f19ab4eb70a5b50948079b1fece97
Jessica <reynolds.jessann@gmail.com>
2020-08-27 20:47:55 -0400

    Answer progress / stuck on Challenge 7

--- index.js
+++ index.js
@@ -54,33 +54,34 @@ let person = {
     }
 }
 */
-const person = [{
-    name: {
-        firstName: 'John',
-        lastName: 'Doe'
-    }
-    car: {
-        make: 'Ford',
-        model: 'Explorer',
-        year: 2019
-    }
-}]
-
-function breakTheNest(items, callback) {
-    for (let i =0; i < items.length; i++) {
-        const item = items[i]
-        
-        callback(item, i, items)
-    }
-}
 /*
-@Challenge 05 - Write a function called 'reverseErase' that takes an array of words, and takes a word and returns the new word without including the first character.
+function breakTheNest() {
+    let person = {
+        name: {
+            firstName: 'John',
+            lastName: 'Doe'
+        },
+        vehicle: {
+            make: 'Ford',
+            model: 'Explorer',
+            year: 2019
+        }
+        
+    }
+    console.log( firstName + ' drives a ' + vehicle)
+}
+breakTheNest()
+/*
+/*
+@Challenge 05 - Write a function called 'reverseErase' that takes an array of words, 
+and takes a word and returns the new word without including the first character.
 @Example - ['Apple', 'Cinnamon', 'Acorn', 'Bread'] should return as: ['pple', 'innamon', 'corn', 'ead']
 @Test - Make a function call of 'reverseErase()'
 */
 
 /*
-@Challenge 06 - Write a function called 'characterSelect' that has an array of characters and you are tasked with having two players select their character.
+@Challenge 06 - Write a function called 'characterSelect' that has an array of characters 
+and you are tasked with having two players select their character.
 @Example - you should get something like: Player one chose Mr Maximus, Player two chose Dr Doof.
 @Test Make a function call of 'characterSelect()'
 let player = [{
@@ -103,18 +104,25 @@ let player = [{
 */
 
 /*
-@Challenge 07 - Write a function called 'Mathmatical' where two numbers are passed as parameters. The first parameter divided by 
+@Challenge 07 - Write a function called 'Mathmatical' where two numbers 
+are passed as parameters. The first parameter divided by 
 the second parameter will have a remainder, possibly zero. Return that value.
 @Example - mathmatical(6,3) should return 2.
 @Test - Make a function call of 'Mathmatical()'
 */
+function Mathmatical(x, y) {
+    return x / y;
 
+}
+console.log(Mathmatical(45, 2))
 /*
 @Challenge 08 - Write a function called 'theSwitcherroo' that can reverse an array.
 @Example - [1,2,3] would return as [3,2,1]
 @Test - Make a function call of 'theSwitcherroo()'
 */
+let arr1 = ['person, woman, man, camera, tv'].reverse();
 
+console.log(arr1)
 /*
 @Challenge 09 - Write a function called 'totalDestruction' that has an array of numbers and your goal is to removes any 
 odd numbers but lets you know if there are only even numbers it will return a message letting the user know.
